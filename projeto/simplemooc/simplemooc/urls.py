fa1706ef702ca2766950136d6ea42a6ee8db450f
Alexandre Azevedo <azvdo.ads@gmail.com>
2018-11-30 20:44:19 -0300

    Capitulo III: Trabalhando com imagens dos cursos

--- projeto/simplemooc/simplemooc/urls.py
+++ projeto/simplemooc/simplemooc/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.conf import settings
+from django.conf.urls.static import static
 
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('simplemooc.core.urls', namespace='core')),
@@ -9,3 +11,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
